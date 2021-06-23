dc50a712003376837bd55c2f08ee1f5077d3eafa
Jarrett Duskey <jarrett@duskey.io>
2021-06-23 19:05:03 -0400

    Initial Project Upload
    This commit adds the local project files to the repo.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-VueMonsterSlayerGame
+#Vue Monster Slayer Game
+This project was created as part of the `Vue - The Complete Guide` course from Udemy (https://www.udemy.com/share/101WzMCUIYdFZSTQ==/)
+
+It is a simple turn-based fighting game built in Vue.
---
+++ app.js
@@ -0,0 +1,109 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+const app = Vue.createApp({
+    data() {
+       return {
+           playerHealth: 100,
+           monsterHealth: 100,
+           currentRound: 0,
+           winner: null,
+           logMessages: []
+       }
+    },
+    methods: {
+        attackMonster() {
+            this.currentRound ++;
+            const attackValue = getRandomValue(5,12);
+            this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack',attackValue);
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8,15);
+            this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
+        },
+        specialAttackMonster() {
+            this.currentRound ++;
+            const attackValue = getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'special attack', attackValue);
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8,20);
+            if (this.playerHealth + healValue > 100){
+                this.playerHealth = 100;
+            }
+            else{
+                this.playerHealth += healValue;
+            }
+            this.addLogMessage('player', 'heal', healValue);
+            this.attackPlayer()
+        },
+        restartGame() {
+            this.currentRound = 0;
+            this.monsterHealth = 100;
+            this.playerHealth = 100;
+            this.winner = null;
+            this.logMessages = [];
+        },
+        surrender(){
+            this.winner = 'monster';
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
+        }
+    },
+    computed: {
+        monsterBarStyles() {
+            let width = 0;
+            if (this.monsterHealth > 0){
+                width = this.monsterHealth;
+            }
+            return {
+                width: width + '%'
+            }
+        },
+        playerBarStyles() {
+            let width = 0;
+            if (this.playerHealth > 0){
+                width = this.playerHealth;
+            }
+            return {
+                width: width + '%'
+            }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0){
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0){
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+    }
+})
+
+app.mount("#game");
